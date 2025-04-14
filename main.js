df63937a7014ab5bd2399d4095d90dfddf8393d3
Rafael Matos <rafaelmatos.programmer@gmail.com>
2025-04-14 20:10:46 +0100

    toggle-dark-mode

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle('dark-mode');
+//DARK AND LIGHT MODE
+const themeToggle = document.getElementById('theme-toggle');
+
+//verificar se há preferência guardada no localStorage para o tema
+const savedTheme = localStorage.getItem('theme');
+if (savedTheme === 'dark') {
+  document.body.classList.add('dark-mode');
+  themeToggle.checked = true;
 }
+
+//Listen if the toggle changed
+themeToggle.addEventListener('change', () => {
+  if (themeToggle.checked) {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark');
+  } else {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('theme', 'light');
+  }
+});
